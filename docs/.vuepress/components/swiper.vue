4402b95336ce3736345e9389680fced4daf7a89a
lzx-xb <321337450@qq.com>
2022-12-20 13:44:21 +0800

    update swiper.vue

---
+++ docs/.vuepress/components/swiper.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <span>{{ content }}</span>
+    </div>
+</template>
+
+<script>
+    import 'animate.css';
+    export default{
+        data() {
+            return {
+                content: 'hello world!',
+            }
+        },
+        methods: {
+            
+        }
+    }
+</script>
+<style > 
+@import '../styles/common.css';
+
+    .container{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
