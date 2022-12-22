0cca6c7a8b6aad01fd61c2a600c66f6c297a79ab
lzx-xb <321337450@qq.com>
2022-12-22 22:26:26 +0800

    add wow.js

--- docs/.vuepress/components/flexbewteen.vue
+++ docs/.vuepress/components/flexbewteen.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <div class="animate__animated animate__bounceInRight">
-            <span >处理前</span>
+        <div class="wow bounceInDown" style="margin: 10px;" >
+            <span>处理前</span>
         </div>
-        <div class="container1 ">
-            <div class="card animate__animated animate__zoomIn" v-for="item in list">
+        <div class="container1">
+            <div class="card wow bounceInRight"  v-for="item in list">
                 <span>{{ item }}</span>
             </div>
         </div>
-        <div class="animate__animated animate__bounceInRight animate__delay-1s">
-            <span >处理后</span>
+        <div class="wow bounceInDown" style="margin: 10px;" >
+            <span>处理后</span>
         </div>
-        <div class="main" >
-            <div class="card animate__animated animate__zoomIn animate__delay-2s" v-for="item in list">
+        <div class="main">
+            <div class="card wow bounceInRight"  v-for="item in list">
                 <span>{{ item }}</span>
             </div>
         </div>
@@ -20,28 +20,42 @@
 </template>
 
 <script>
-import 'animate.css';
-export default{
+// import 'animate.css';
+import '../wowjs/animate.css';
+import  WOW  from '../wowjs/WOW.js';
+export default {
     data() {
         return {
-            list: ['1','2','3','4','5']
+            list: ['1', '2', '3', '4', '5']
         }
+    },
+    mounted() {
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 40,
+            mobile: true,
+            live: true,
+        })
+        wow.init()
     }
 }
 </script>
 
 <style>
-.container1, .main{
+.container1,.main {
     display: flex;
     align-items: center;
     flex-wrap: wrap;
     justify-content: space-between;
 }
-.container::after{
+
+.main::after {
     width: 30%;
     content: '';
 }
-.card{
+
+.card {
     width: 30%;
     height: 200px;
     background-color: #ddffdd;
