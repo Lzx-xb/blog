2961a04957dd104d76b57df1a230182f7ec2487e
Lzx-xb <67529621+Lzx-xb@users.noreply.github.com>
2022-12-22 23:49:02 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,20 +1,13 @@
 
 //import "./wowjs/animate.css";
-import  WOW  from './wowjs/WOW.js';
+//import  WOW  from './wowjs/WOW.js';
 export default ({ Vue, options, router, siteData }) => {
    Vue.mixin({
        mounted() {
-        var wow = new WOW({
-            boxClass: 'wow',
-            animateClass: 'animated',
-            offset: 40,
-            mobile: true,
-            live: true,
-        })
-        wow.init()
-        //    import('./wowjs/WOW.js').then(function (m) {
-        //        Vue.use(m.default)
-        //    })
+        
+        import('./wowjs/WOW.js').then(function (m) {
+               Vue.use(m.default)
+         })
        },
    })
 };
