604f53a0f23a2309056b6911b598889fe174ea5d
lzx-xb <321337450@qq.com>
2022-12-22 23:13:44 +0800

    update docs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,12 +1,20 @@
 
 import "wowjs/css/libs/animate.css";
-
+import  WOW  from './wowjs/WOW.js';
 export default ({ Vue, options, router, siteData }) => {
    Vue.mixin({
        mounted() {
-           import('wowjs').then(function (m) {
-               Vue.use(m.default)
-           })
+        var wow = new WOW({
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 40,
+            mobile: true,
+            live: true,
+        })
+        wow.init()
+        //    import('./wowjs/WOW.js').then(function (m) {
+        //        Vue.use(m.default)
+        //    })
        },
    })
 };
